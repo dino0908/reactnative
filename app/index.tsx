24a3ed1cac5372489441a2244869dee0d762b566
dino0908 <dinokhaw@yahoo.com.sg>
2024-12-01 18:41:29 +0800

    Add native base component library, todo app home screen

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,30 @@
-import { Text, View } from "react-native";
+import { NativeBaseProvider, Input, Progress, HStack, Button } from "native-base";
+import { SafeAreaView, Text } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>test</Text>
-    </View>
+    <NativeBaseProvider>
+      <SafeAreaView
+        style={{
+          flex: 1,
+        }}
+      >
+        <Text
+          style={{
+            marginTop: 20,
+            textAlign: "center",
+            fontWeight: "bold",
+            fontSize: 24,
+          }}
+        >
+          To Do App
+        </Text>
+        <HStack space={3} justifyContent="center" marginTop={5}>
+        <Input placeholder="Input" w="60%"  height={"40px"}/>
+        <Button height={"40px"} onPress={() => console.log("hello world")}>Click Me</Button>
+        </HStack>
+        
+      </SafeAreaView>
+    </NativeBaseProvider>
   );
 }
