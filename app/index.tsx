fe4bad090eadedc4c5e78b54cdc56a645c326ede
dino0908 <dinokhaw@yahoo.com.sg>
2024-12-01 19:31:10 +0800

    add and display to do tasks

--- app/index.tsx
+++ app/index.tsx
@@ -1,30 +1,43 @@
-import { NativeBaseProvider, Input, Progress, HStack, Button } from "native-base";
-import { SafeAreaView, Text } from "react-native";
+import { NativeBaseProvider, Input, HStack, Button, VStack, Text } from "native-base";
+import { SafeAreaView } from "react-native";
+import { useState } from "react";
+
 
 export default function Index() {
+
+  const [tasks, setTasks] = useState<string[]>([])
+  const [taskInput, setTaskInput] = useState<string>("")
+
+  const addTask = () => {
+    if (taskInput.trim()) {
+      setTasks([...tasks, taskInput]);
+      setTaskInput("")
+    }
+  }
   return (
     <NativeBaseProvider>
       <SafeAreaView
-        style={{
-          flex: 1,
-        }}
       >
         <Text
-          style={{
-            marginTop: 20,
-            textAlign: "center",
-            fontWeight: "bold",
-            fontSize: 24,
-          }}
+        fontSize="2xl"
+        bold
+        textAlign="center"
+        marginTop={5}
         >
           To Do App
         </Text>
         <HStack space={3} justifyContent="center" marginTop={5}>
-        <Input placeholder="Input" w="60%"  height={"40px"}/>
-        <Button height={"40px"} onPress={() => console.log("hello world")}>Click Me</Button>
+        <Input placeholder="Enter task name" w="60%"  height={"40px"} value={taskInput} onChangeText={(text) => setTaskInput(text)}/>
+        <Button height={"40px"} onPress={addTask}>Add</Button>
         </HStack>
-        
+
+        <VStack space={3} alignItems="center" marginTop={10}>
+          {tasks.map((task, index) => (
+            <Text key={index} fontSize={18}>{task}</Text>
+          ))}
+        </VStack>
       </SafeAreaView>
     </NativeBaseProvider>
   );
+
 }
